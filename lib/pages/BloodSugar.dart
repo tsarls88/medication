4b8837d175f48f4cf33dc168f61c8bbf2edee63f
Charles Hinay <hinaycharles12322@gmail.com>
2023-10-19 20:17:32 +0800

    minor changes

---
+++ lib/pages/BloodSugar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BloodSugar extends StatelessWidget {
+  const BloodSugar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('BLOOD SUGAR'),
+      ),
+    );
+  }
+}
